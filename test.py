b04fa4deea894793ef8b7ce5657d0b475ac48c60
slayerzeroa <slayerzeroa@naver.com>
2024-08-19 22:17:42 +0900

    feat: test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+# Monthly Data to Daily Data
 def repeat_data(data, repeat):
     result = []
     for i in data:
@@ -10,10 +11,12 @@ def repeat_data(data, repeat):
             result.append(i)
     return result
 
+# Calculate Moving Average
 def cal_MA(data, window):
     MA = np.mean(data[-window+1:])
     return MA
 
+# Calculate Change
 def cal_change(d1, d2):
     change = (d1 - d2) / d2
     return change
@@ -28,10 +31,10 @@ pce = [0.02, 0.03, 0.05, 0.04, 0.07, 0.03, 0.02, 0.04, 0.01, 0.02, 0.02, 0.02]
 core_cpi, core_pce = repeat_data(core_cpi, 30), repeat_data(core_pce, 30)
 cpi, pce = repeat_data(cpi, 30), repeat_data(pce, 30)
 
-gasoline_timeline = [60 for i in range(14*30+1)] + np.cumsum(np.random.normal(0, 3, 14*30+1))
-oil_timeline = [70 for i in range(14*30+1)] + np.cumsum(np.random.normal(0, 3, 14*30+1))
-food_cpi = [30 for i in range(14*30+1)] + np.cumsum(np.random.normal(0, 3, 14*30+1))
-food_off_pce = [20 for i in range(14*30+1)] + np.cumsum(np.random.normal(0, 3, 14*30+1))
+gasoline_timeline = [60 for i in range(14*30+1)] + np.cumsum(np.random.normal(0, 1, 14*30+1))
+oil_timeline = [70 for i in range(14*30+1)] + np.cumsum(np.random.normal(0, 1, 14*30+1))
+food_cpi = [30 for i in range(14*30+1)] + np.cumsum(np.random.normal(0, 1, 14*30+1))
+food_off_pce = [20 for i in range(14*30+1)] + np.cumsum(np.random.normal(0, 1, 14*30+1))
 
 chg_gasoline = []
 chg_oil = []
@@ -43,8 +46,6 @@ for i in range(len(gasoline_timeline)-1):
     chg_oil.append(cal_change(oil_timeline[i+1], oil_timeline[i]))
     chg_food_cpi.append(cal_change(food_cpi[i + 1], food_cpi[i]))
     chg_food_off_pce.append(cal_change(food_off_pce[i + 1], food_off_pce[i]))
-
-
 
 # X = np.array(core_cpi[0:360]).reshape(-1, 1)
 # y = np.array(core_pce[0:360]).reshape(-1, 1)
@@ -79,9 +80,6 @@ for i in range(len(gasoline_timeline)-1):
 # predict_pce = lr.predict(np.array([[predict_core_pce, gasoline_timeline[360], food_off_pce[360]]]))[0][0]
 # pce.append(predict_pce)
 
-
-
-
 for i in range(59):
     # core PCE 회귀
     X = np.array(core_cpi[i:360+i]).reshape(-1, 1)
@@ -95,6 +93,7 @@ for i in range(59):
 
     predict_core_pce = lr.predict(np.array([[predict_core_cpi]]))[0][0]
     core_pce.append(predict_core_pce)
+    core_pce[-1]=cal_MA(core_pce, 12*30)
 
     # CPI 회귀
     X = np.array([np.array(core_cpi[i:360+i]), np.array(chg_gasoline[i:360+i]),np.array(chg_food_cpi[i:360+i])]).reshape(-1, 3)
@@ -105,6 +104,7 @@ for i in range(59):
 
     predict_cpi = lr.predict(np.array([[predict_core_cpi, chg_gasoline[361+i], chg_food_cpi[361+i]]]))[0][0]
     cpi.append(predict_cpi)
+    cpi[-1]=cal_MA(cpi, 12*30)
 
     # PCE 회귀
     X = np.array([np.array(core_pce[i:360+i]), np.array(chg_gasoline[i:360+i]),np.array(chg_food_off_pce[i:360+i])]).reshape(-1, 3)
@@ -116,7 +116,7 @@ for i in range(59):
 
     predict_pce = lr.predict(np.array([[predict_core_pce, chg_gasoline[361+i], chg_food_off_pce[361+i]]]))[0][0]
     pce.append(predict_pce)
-
+    pce[-1]=cal_MA(pce, 12*30)
 
 plt.figure(figsize=(20, 10))
 plt.plot(cpi, label='CPI')
